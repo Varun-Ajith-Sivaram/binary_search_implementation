83829f90d3f3065ff1d026b2a43ae395ab682328
Varun Ajith Sivaram <varunajith2112@gmail.com>
2021-07-16 19:20:00 +0530

    Update binary_search.c

--- binary_search.c
+++ binary_search.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
 
-#define size 10 //size of array is initialized to 10
-
-void sort(int[]);    //function to sort the array in increasing order
+void Binary_Search(int[],int,int);
+void sort(int[],int);    //function to sort the array in increasing order
 
 void main()
 {
-    int a[size],element,beg,mid,end,flag=0;
+    int element,beg,mid,end,flag=0;
 
     printf("\tBinary Search Implementation\n");
+    
+    int size;
+    
+    printf("\nEnter the number of elements: ");
+    scanf("%d",&size);
+    
+    int a[size];
     
     printf("\nEnter the elements:\n");
 
@@ -17,7 +23,7 @@ void main()
         scanf("%d",&a[i]);
     }
     
-    sort(a);    //sorting the array; binary search works only on a sorted array
+    sort(a,size);    //sorting the array; binary search works only on a sorted array
     
     printf("\nElements in array after sorting: ");
     for(int i=0;i<size;i++)
@@ -29,8 +35,32 @@ void main()
     printf("\n Enter the element to be searched: ");
     scanf("%d",&element);
     
-    beg=0;
-    end=size-1;
+    Binary_Search(a,element,size);
+}
+
+
+
+void sort(int a[],int n)
+{
+    int temp;
+    
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+}
+
+void Binary_Search(int a[],int element,int n)
+{
+    int mid,flag=0,beg=0,end=n-1;
     
     while(beg<=end)
     {
@@ -53,25 +83,4 @@ void main()
 
     if(flag==0)
         printf("\n   --> Searched element not found");
-
-}
-
-
-
-void sort(int a[])
-{
-    int temp;
-    
-    for(int i=0;i<size-1;i++)
-    {
-        for(int j=i+1;j<size;j++)
-        {
-            if(a[i]>a[j])
-            {
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
-            }
-        }
-    }
 }
